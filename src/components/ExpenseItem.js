e317e9653a429887826129e2ed7d3d5efbe126ef
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-05-20 19:19:14 -0700

    Added a few new components practicing passing props to child components.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,18 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+
 const ExpenseItem = (props) => {
+  
+
   return (
-    <div>
-      <h3>Expense Item: {props.name}.</h3>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>Expense Item: {props.title}.</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ExpenseItem;
---
+++ src/components/Expenses.js
@@ -0,0 +1,20 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <div className="expenses">
+      {props.expenses.map(expense => {
+        return (
+          <ExpenseItem 
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Expenses;
