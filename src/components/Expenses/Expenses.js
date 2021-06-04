1a28a3a515a0205b485bf3cc88fc320290813693
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-06-04 11:26:39 -0700

    Added ExpenseFilter component.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,35 @@
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
 import Card from '../UI/Card';
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [expenseFilter, setExpenseFilter] = useState('2020');
+
+  const expenseFilterHandler = (event) => {
+    setExpenseFilter(() => event.target.value);
+  }
+
   return (
-    <Card className="expenses">
-      {props.expenses.map(expense => {
-        return (
-          <ExpenseItem 
-            key={expense.title}
-            title={expense.title}
-            date={expense.date}
-            amount={expense.amount}
-          />
-        );
-      })}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter 
+          selected={expenseFilter}
+          changed={expenseFilterHandler} 
+        />
+        {props.expenses.map(expense => {
+          return (
+            <ExpenseItem 
+              key={expense.title}
+              title={expense.title}
+              date={expense.date}
+              amount={expense.amount}
+            />
+          );
+        })}
+      </Card>
+    </div>
   );
 };
 
